1dded391b1e08f8c685ab3831dc57a5f259412c3
Nathalie Zwimpfer <nathaliezwimpfer@Nathalies-MBP.fios-router.home>
2021-05-07 13:33:33 -0400

    Make various updates to the app collection

--- script.js
+++ script.js
@@ -1,62 +1,80 @@
+initiateColorFlipper();
+initiateChatApp();
+// initiatePasswordGenerator();
 //Color Flipper
+function initiateColorFlipper() {
+  // when clicking on button that has 'btn' ID, then execute function changeBGAndNumber
+  let button = document.getElementById('btn');
 
-// when clicking on button that has 'btn' ID, then execute function changeBGAndNumber
-console.log('A'); // TODO THIS COMMIT: remove this
-let button = document.getElementById('btn');
+  button.addEventListener('click', changeBGAndNumber);
 
-button.addEventListener('click', changeBGAndNumber);
-
-// changes number of HEX and backgroundcolor of app's container
-function changeBGAndNumber() {
-  console.log('B'); // TODO THIS COMMIT: remove this
-  let randomColor = getHexString();
-  turnBGColorToRandomColor(randomColor);
-  changeTextOfHex(randomColor);
-}
-
-let bGColor = document.getElementById('color-flipper-container');
-let hexText = document.getElementById('hex-color');
-
-// 6 random letters and numbers need to be generated and put into a string.
-function getHexString() {
-  let hexString = '';
-  // Array of letters and numbers
-  for (var index = 0; index < 6; index++) {
-    const numbersAndLetters = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      0,
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9
-];
-    hexString = hexString.concat(getRandomLetterOrNumber(numbersAndLetters));
+  // changes number of HEX and backgroundcolor of app's container
+  function changeBGAndNumber() {
+    let randomColor = getHexString();
+    turnBGColorToRandomColor(randomColor);
+    changeTextOfHex(randomColor);
   }
-  return hexString.toUpperCase();
+
+  let bGColor = document.getElementById('color-flipper-container');
+  let hexText = document.getElementById('hex-color');
+
+  // 6 random letters and numbers need to be generated and put into a string.
+  function getHexString() {
+    let hexString = '';
+    // Array of letters and numbers
+    for (var index = 0; index < 6; index++) {
+      const numbersAndLetters = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+  ];
+      hexString = hexString.concat(getRandomLetterOrNumber(numbersAndLetters));
+    }
+    return hexString.toUpperCase();
+  }
+
+  function getRandomLetterOrNumber(numbersAndLetters) {
+    let randomIndex = Math.floor(Math.random() * numbersAndLetters.length);
+    return numbersAndLetters[randomIndex];
+  }
+
+  function turnBGColorToRandomColor(randomColor) {
+    bGColor.style.backgroundColor = '#'.concat(randomColor);
+  }
+
+  function changeTextOfHex(randomColor) {
+    hexText.innerHTML = randomColor;
+  }
 }
 
-function getRandomLetterOrNumber(numbersAndLetters) {
-  let randomIndex = Math.floor(Math.random() * numbersAndLetters.length);
-  return numbersAndLetters[randomIndex];
-}
+function initiateChatApp() {
+  let inputMessage = document.getElementById('message').value;
+  let sendButton = document.getElementById('send-button');
+  let discussionBox = document.getElementById('discussion-box');
 
-function turnBGColorToRandomColor(randomColor) {
-  bGColor.style.backgroundColor = '#'.concat(randomColor);
+  sendButton.addEventListener('click', function (inputMessage) {
+    console.log(inputMessage); // TODO THIS COMMIT: remove this
+    discussionBox.innerHTML = inputMessage;
+  });
 }
-
-function changeTextOfHex(randomColor) {
-  hexText.innerHTML = randomColor;
-}
-
 
 // Random Password Generator
+// when clicking on button a random password of 10 random numbers, letters and special characters is created
+function initiatePasswordGenerator() {
+  let pwButton = document.getElementById('password-btn');
+
+  pwButton.addEventListener('click', createPw);
+}
